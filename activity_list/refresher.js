5f505e9114197a4a13ecdec715768874a8bde967
mclee3360 <mclee3360@gmail.com>
2018-01-21 02:24:19 -0500

    Add manual and automatic refresher of custom function update

--- activity_list/refresher.js
+++ activity_list/refresher.js
@@ -1,7 +1,15 @@
-
-
-function onOpen()
+function refresh()
 {
     SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Refresh')
         .getRange(1,1).setValue(new Date().getTime())
+}
+
+function onOpen()
+{
+    var sheet = SpreadsheetApp.getActiveSpreadsheet();
+    var entries = [{
+        name : "Refresh",
+        functionName : "refresh"
+    }];
+    sheet.addMenu("Refresh", entries);
 }
