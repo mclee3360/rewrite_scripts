83d555615637d1542914b7d15a72c79a17b3810d
mclee3360 <mclee3360@gmail.com>
2018-01-21 20:33:42 -0500

    Add counting for phase one and phase two edits

--- activity_list/editor_activity.js
+++ activity_list/editor_activity.js
@@ -1,3 +1,45 @@
+function getNumPhaseOne(name_cell1, name_cell2)
+{
+    return getNumEdits(getUsernames(name_cell1, name_cell2), 1);
+}
+
+function getNumPhaseTwo(name_cell1, name_cell2)
+{
+    return getNumEdits(getUsernames(name_cell1, name_cell2), 2);
+}
+
+function getNumEdits(usernames, phase)
+{
+    var col = 0;
+    if (phase == 1)
+    {
+        col = p1Editor;
+    }
+    else if (phase == 2)
+    {
+        col = p2Editor;
+    }
+    else
+    {
+        return -1;
+    }
+    var archive = getArchiveRange();
+    // Count number of synopses by user.
+    var count = 0;
+    for (i = 0; i < archive.length; i++)
+    {
+        for (j = 0; j < usernames.length; j++)
+        {
+            var checkEditor = ' ' + archive[i][col - 1].toLowerCase() + ' ';
+            if (checkEditor.indexOf(' ' + usernames[j] + ' ') >= 0)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 function getActivityEditor(name_cell1, name_cell2)
 {
     var usernames = getUsernames(name_cell1, name_cell2);
