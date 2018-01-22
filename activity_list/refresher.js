6b5259ba14181b77afd2da93b4f1f9ca2b74db81
mclee3360 <mclee3360@gmail.com>
2018-01-21 20:59:36 -0500

    Add refreshing options for coordinator and retirees. Also added daily trigger

--- activity_list/refresher.js
+++ activity_list/refresher.js
@@ -1,24 +1,49 @@
+function daily_refresh()
+{
+    refresh_writer();
+    refresh_editor();
+    refresh_coord();
+}
+
 function refresh_writer()
 {
     SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Refresh')
-        .getRange(1,1,1,1).setValue(new Date().getTime())
+        .getRange(1,2,1,1).setValue(new Date().getTime())
 }
 
 function refresh_editor()
 {
     SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Refresh')
-        .getRange(1,2,1,1).setValue(new Date().getTime())
+        .getRange(2,2,1,1).setValue(new Date().getTime())
+}
+
+function refresh_coord()
+{
+    SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Refresh')
+        .getRange(3,2,1,1).setValue(new Date().getTime())
+}
+
+function refresh_retired()
+{
+    SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Refresh')
+        .getRange(4,2,1,1).setValue(new Date().getTime())
 }
 
 function onOpen()
 {
     var sheet = SpreadsheetApp.getActiveSpreadsheet();
     var entries = [{
-        name : "Refresh",
+        name : "Refresh Writers",
         functionName : "refresh_writer"
     }, {
         name : "Refresh Editors",
         functionName : "refresh_editor"
+    }, {
+        name: "Refresh Coordinators",
+        functionName : "refresh_coord"
+    }, {
+        name: "Refresh Retirees",
+        functionName: "refresh_retired"
     }];
     sheet.addMenu("Refresh", entries);
 }
